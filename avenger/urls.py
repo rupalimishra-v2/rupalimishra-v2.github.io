2a1d666c82af8ad3a10bdf9172bb38282fe92202
rupalimishra-v2 <rupali.mishra304@gmail.com>
2021-08-06 23:17:34 +0530

    Adding static website on myself

---
+++ avenger/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from avenger import views
+
+urlpatterns = [
+    path('', views.about, name='about'),
+    path("projects/", views.project_index, name="project_index"),
+    path("projects/<int:pk>/", views.project_detail, name="project_detail"),
+]
---
+++ avenger/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from avenger.models import Project
+
+
+def about(request):
+    return render(request, 'about.html', {})
+
+
+def project_index(request):
+    projects = Project.objects.all()
+    context = {
+        'projects': projects
+    }
+    return render(request, 'project_index.html', context)
+
+
+def project_detail(request, pk):
+    project = Project.objects.get(pk=pk)
+    context = {
+        'project': project
+    }
+    return render(request, 'project_detail.html', context)
